8da73c37385d408ad1ec62c5363dbab3933fce2d
Ivan Ray Altomera <irayspacii@gmail.com>
2020-01-30 14:50:22 +0800

    feat: intuit oauth2

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express = require('express')
+const express = require('express');
+const OAuthClient = require('intuit-oauth');
 
-// @configurations
+// @configurationsq
 const PORT = process.env.PORT || 3000;
+const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID;
+const OAUTH_CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
 
 // @app
-const app = express()
+const app = express();
 app.get('/', (req, res) => res.send('intuit-oauth2-express by iRaySpace'));
+app.get('/auth', (req, res) => {
+    const oauthClient = new OAuthClient({
+        clientId: OAUTH_CLIENT_ID,
+        clientSecret: OAUTH_CLIENT_SECRET,
+        environment: 'sandbox',
+        redirectUri: 'https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl'
+    });
+    const authUri = oauthClient.authorizeUri({
+        scope: [OAuthClient.scopes.Accounting]
+    });
+    res.redirect(authUri);
+});
+app.get('/authRedirect', (req, res) => {
+    res.send('in progress');
+});
+
+
 app.listen(PORT, () => console.log('App listening'));
